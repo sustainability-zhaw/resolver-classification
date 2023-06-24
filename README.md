65cd3f711f8c28ca794912de24eabad6ba5b9c99
Davide Stallone <stlo@zhaw.ch>
2023-06-24 16:18:13 +0200

    Refactor

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Resolver Classification
 
-Find classifications (ddcs) that were incorrectly written to the `keywords` field and move them to `class` field.
+Finds classifications (ddcs) that were incorrectly written to the `keywords` field of an info_objects and moves them to the `class` field.
 
-## Development
+## Configuration
 
-- ### TODO
+The following can be configured with environment variables or by providing a JSON file at `/etc/app/config.json`.
+An additional JSON file is read from `/etc/app/secrets.json` that can be used for sensitive information.
 
-## Configuration Variables
+*Note: Config files take precedence over environment variables.*
 
-The main service configuration is set via environment variables. 
-
-- `DB_HOST` - host or IP of the graphQL endpoint
-- `LOG_LEVEL` - log level for tracing and debugging, default is `ERROR`
+- `DB_HOST` - GraphQL host. Defaults to `localhost:8080`.
+- `MQ_HOST` - RabbitMQ host. Defaults to `mq`.
+- `MQ_EXCHANGE` - RabbitMQ exchange name. Defaults to `zhaw-km`.
+- `MQ_QUEUE` - RabbitMQ queue name, Defaults to `classificationqueue`.
+- `MQ_BINDKEYS` - RabbitMQ routing keys to use to bind the queue to the exchange. Defaults to `importer.object`. Multiple routing keys can be provided with comma separated string.
+- `MQ_HEARTBEAT` - RabbitMQ heart beat in seconds. Defaults to `6000` (1.6h).
+- `MQ_TIMEOUT` - RabbitMQ blocking timeout in seconds. Defaults to `3600` (1h).
+- `LOG_LEVEL` - Log level for tracing and debugging. Defaults to `ERROR`.
---
+++ src/resolver.py
@@ -0,0 +1,93 @@
+import logging
+import re
+
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+
+from settings import settings
+
+
+logger = logging.getLogger(__name__)
+
+graphql_client = Client(transport=RequestsHTTPTransport(url=f"http://{settings.DB_HOST}/graphql"))
+
+
+def query_info_object_by_link(link):
+    return graphql_client.execute(
+        gql(
+            """
+            query getInfoObject($link: String!) {
+                getInfoObject(link: $link) {
+                    link
+                    keywords {
+                        name
+                    }
+                    class {
+                        id
+                        name
+                    }
+                }
+            }
+            """
+        ),
+        variable_values={ "link": link }
+    )["getInfoObject"]
+
+
+def update_info_object(input):
+        graphql_client.execute(
+            gql(
+                """
+                mutation updateInfoObject($input: UpdateInfoObjectInput!) {
+                    updateInfoObject(input: $input) {
+                        infoObject {
+                            link
+                        }
+                    }
+                }
+                """
+            ),
+            variable_values={
+                  "input": input
+            }
+        )
+
+
+def run(link):
+    logging.info(f"Resolving classifications for link {link}")
+
+    info_object = query_info_object_by_link(link)
+
+    if not info_object:
+        return
+    
+    keywords_to_remove = []
+    classifications = []
+
+    for keyword in info_object["keywords"]:
+        is_classification = bool(re.match(r"(?:\d{3}\.\d+|\d{3}):", keyword["name"]))
+        if is_classification:
+            keywords_to_remove.append(keyword)
+            values = keyword["name"].split(":")
+            classifications.append({ "id": values[0].strip(), "name": values[1].strip() })
+
+    info_object_classifications = { classification["id"] for classification in info_object["class"] }
+
+    new_classifications = [
+        classification
+        for classification in classifications 
+        if classification["id"] not in info_object_classifications
+    ]
+
+    logger.info(f"Found {len(new_classifications)} classifications in keywords field")
+    
+    if not (len(keywords_to_remove) + len(classifications)):
+        return
+
+    update_info_object(
+        {
+            "filter": { "link": { "eq": info_object["link"] } },
+            "set": { "class": new_classifications },
+            "remove": { "keywords": keywords_to_remove }
+        }
+    )
